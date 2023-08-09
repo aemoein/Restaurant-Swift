dbacaa0d46e2bab957339a124ac3460a1c1b3dd6
Ahmed Elsayed <ahmed33elsayed22@gmail.com>
2023-08-09 07:27:37 +0300

    Menu Items
    the menu items page is finished however an error with the vertical alignment of the details label persists

--- OrderApp/OrderApp/MenuItemDetailViewController.swift
+++ OrderApp/OrderApp/MenuItemDetailViewController.swift
@@ -9,6 +9,34 @@ import UIKit
 
 @MainActor
 class MenuItemDetailViewController: UIViewController {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var detailTextLabel: UILabel!
+    @IBOutlet weak var addToOrderButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        nameLabel.text = menuItem.name
+        priceLabel.text = menuItem.price.formatted(.currency(code: "usd"))
+        detailTextLabel.text = menuItem.detailText
+    }
+    
+    @IBAction func orderButtonTapped(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.5, delay: 0,
+           usingSpringWithDamping: 0.7, initialSpringVelocity: 0.1,
+           options: [], animations: {
+            self.addToOrderButton.transform =
+               CGAffineTransform(scaleX: 2.0, y: 2.0)
+            self.addToOrderButton.transform =
+               CGAffineTransform(scaleX: 1.0, y: 1.0)
+        }, completion: nil)
+    }
     
     let menuItem: MenuItem
     
