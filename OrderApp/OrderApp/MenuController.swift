e8b0794d41c254529aaeb08462710126d1987c14
Ahmed Elsayed <ahmed33elsayed22@gmail.com>
2023-08-09 07:52:41 +0300

    View And Edit Order
    Users can now view and edit orders on the app they can add them to the car and remove them

--- OrderApp/OrderApp/MenuController.swift
+++ OrderApp/OrderApp/MenuController.swift
@@ -9,6 +9,13 @@ import Foundation
 
 class MenuController {
     static let shared = MenuController()
+    var order = Order() {
+        didSet {
+            NotificationCenter.default.post(name: MenuController.orderUpdatedNotification, object: nil)
+        }
+    }
+    
+    static let orderUpdatedNotification = Notification.Name("MenuController.orderUpdated")
 
     let baseURL = URL(string: "http://localhost:8080/")!
     
